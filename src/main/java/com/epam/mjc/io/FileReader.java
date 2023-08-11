58a60248d0b82b897babf880ba4868b4cdf3941a
Denis Stepshin <Evil9493@ya.ru>
2023-08-11 15:10:47 +0600

    Completed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,45 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> profileData = getProfileData(file);
+        return createProfileFromData(profileData);
+    }
+
+    private static Map<String, String> getProfileData(File file) {
+        Map<String, String> profileData = new HashMap<>();
+        try (var reader = new BufferedReader(new java.io.FileReader(file))) {
+            while (reader.ready()) {
+                var line = reader.readLine();
+                String[] data = line.split(": ");
+                profileData.put(data[0], data[1]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Error occured while reading file");
+            throw new RuntimeException(e);
+        }
+        return profileData;
+    }
+
+    private Profile createProfileFromData(Map<String, String> profileData) {
+        String name = profileData.get("Name");
+        String email = profileData.get("Email");
+        int age = Integer.parseInt(profileData.get("Age"));
+        long phone = Long.parseLong(profileData.get("Phone"));
+        return new Profile(name, age, email, phone);
+    }
+
+    public static void main(String[] args) {
+        var fileReader = new FileReader();
+        System.out.println(fileReader.getDataFromFile(new File("src/main/resources/Profile.txt")));
     }
 }
